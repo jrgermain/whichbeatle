f15f89727b8753c19f71fd894aea799df4a5197d
jrgermain <jrgermain@github.com>
2019-01-04 18:51:55 -0500

    move System.exit outside of displayUsage
    It makes more sense to explicitly call the exit function after showing usage info instead of bundling the two together. Now displayUsage doesn't have to take any parameters.

--- src/WhichBeatle.java
+++ src/WhichBeatle.java
@@ -18,19 +18,19 @@ public class WhichBeatle {
         String searchKey = "";
 		// Process flags and build search string
 		for (String s : input) {
-			/* The help flag calls the displayUsage function which will also quit
-			 * the application. This means calling help will skip all other flags 
-			 * to present the user with usage instructions.
+			/* The help flag calls the displayUsage function and quits the
+			 * application. This will skip all other flags.
 			 */
 			if (s.equals("--help") || s.equals("-h")) {
-                displayUsage(0);
+                displayUsage();
+                System.exit(0);
 			}
 			
 			/* The rest of the flags below can be stacked -- e.g. the user can search 
 			 * for both the writer and the album the song appeared on using "-wa".
 			 * Flags starting with a single '-' can be combined arbitrarily, but flags
 			 * starting with "--" must be separated. (-wa == -aw == -a -w) (--singer 
-			 * --album is valid, but --singeralbum is invalid) If the input isn't a 
+			 * --album is valid, but --singeralbum is invalid) If the input isn't a
 			 * valid flag, then add it to our search string.
 			 */
 			if (s.charAt(0) == '-' && s.charAt(1) != '-') {
@@ -74,20 +74,19 @@ public class WhichBeatle {
 		// If our search is empty, then show an error
 		if (searchKey.length() == 0) {
             System.err.println("Error: no search criteria specified");
-            displayUsage(1);
+            displayUsage();
+            System.exit(1);
 		}
 
         return searchKey;
     }
 
-    private static void displayUsage(int exitCode) {
+    private static void displayUsage() {
 		System.out.println("Usage: whichbeatle [option] [songname]");
 		System.out.println("Option");
 		System.out.println("  -w, --wrote\t display who wrote the song");
 		System.out.println("  -s, --sang \t display who sang the song");
 		System.out.println("  -a, --album\t display the album on which the song first appeared");
-		// Quit the program, giving exit code 1 for error and 0 for success
-        System.exit(exitCode);
     }
 
     private static void search(String key) {
